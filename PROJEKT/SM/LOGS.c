5009e6542a596f7b7bc4a37fd86e775b3cd259b9
Krasa35 <kacper.krasin35@gmail.com>
2024-01-13 20:51:07 +0100

    Added Logs

--- PROJEKT/SM/LOGS.c
+++ PROJEKT/SM/LOGS.c
@@ -1,7 +1,14 @@
-jklasdjjsak 
-sa
-default 
-as 
-;
-asdd
-defaultjd.exe
+#include <stdio.h>
+int main() {
+    double a, b, product;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    // Calculating product
+    product = a * b;
+
+    // %.2lf displays number up to 2 decimal point
+    printf("Product = %.2lf", product);
+    
+    return 0;
+}
